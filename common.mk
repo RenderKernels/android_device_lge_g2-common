82270524e5a8f4d104565218f973a0ae5079f3c0
Snuzzo <jrracinfan@gmail.com>
2015-11-01 12:30:05 -0500

    Remove DTB DIR declare

--- common.mk
+++ common.mk
@@ -3,6 +3,5 @@ CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
 ARCH := arm
 ZIP_FILES_DIR := device/lge/g2-common/zip_files
 TARGET_REQUIRES_DTB := true
-DTB_DIR := arch/arm/boot
 
 include vendor/render/configs/common.mk
