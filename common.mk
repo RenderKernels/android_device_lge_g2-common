28966bdc895323f5b26afe5c10b322542e22c155
Snuzzo <jrracinfan@gmail.com>
2015-11-01 11:55:53 -0500

    Move ZIMAGE definition

--- common.mk
+++ common.mk
@@ -1,7 +1,6 @@
 PRODUCT_KERNEL_SOURCE := kernel/lge/msm8974-caf
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
 ARCH := arm
-ZIMAGE := arch/arm/boot/zImage
 ZIP_FILES_DIR := device/lge/g2-common/zip_files
 TARGET_REQUIRES_DTB := true
 DTB_DIR := arch/arm/boot
