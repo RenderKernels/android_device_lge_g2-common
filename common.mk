ae90fb70b49391f8f5b79e4b708b4e55e164abb9
Snuzzo <jrracinfan@gmail.com>
2015-11-07 15:45:58 -0500

    Revert "ARCH: Use default define"
    This reverts commit ad7317e264d0abeaf5f48922f07e47a8724c9f37.

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 PRODUCT_KERNEL_SOURCE := kernel/lge/msm8974-caf
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
+ARCH := arm
 ZIP_FILES_DIR := device/lge/g2-common/zip_files
 TARGET_REQUIRES_DTB := true
 
